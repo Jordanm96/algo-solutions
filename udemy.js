05993ae75298ef075fee5db2a919004607526194
Jordan Montero <jordmontero96@gmail.com>
2021-06-17 14:31:20 -0700

    Fizzbuzz

--- udemy.js
+++ udemy.js
@@ -78,3 +78,29 @@ function maxChar(str) {
   return maxChar
 }
 console.log(maxChar("abcccd"))
+// ----------------------------------------BREAK----------------------------------------
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log("FIZZBUZZ")
+    } else if (i % 5 === 0) {
+      console.log("BUZZ")
+    } else if (i % 3 === 0) {
+      console.log("FIZZ")
+    } else console.log(i)
+  }
+}
+console.log(fizzBuzz(16))
