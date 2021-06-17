392858722aa96a64f3de4efe424d82cf4a83d23a
Jordan Montero <jordmontero96@gmail.com>
2021-06-17 09:40:49 -0700

    Isogram

--- solutions.js
+++ solutions.js
@@ -398,4 +398,17 @@ function arrayDiff(a, b) {
   // Another example of using filter
   // let solution = a.filter((num) => num > 1);
 }
-console.log(arrayDiff([1,2,3], [3]))
+// console.log(arrayDiff([1, 2, 3], [3]))
+
+// ISOGRAM 
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+function isIsogram(str) {
+  let arr = str.toLowerCase().split("").sort()
+  // console.log(arr)
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == arr[i + 1])
+      return false
+    else return true
+  }
+}
+console.log(isIsogram("sweet"))
